1993310a85a4b9c16ea7021b2ef6f6679b16b7a8
loditf <101756672+loditf@users.noreply.github.com>
2022-04-13 15:51:46 +0200

    Add files via upload

---
+++ Exercice1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+#importation du fichier csv pour convertir en dataframe et traiter les données
+df_chomage = pd.read_csv("https://www.bfs.admin.ch/bfsstatic/dam/assets/20964153/master", sep = ';')
+#verfier s'il y a des données pour toutes les années (2010 à 2020)
+print(df_chomage)
+for i in range(2010, 2021):
+    if any(df_chomage.TIME_PERIOD == i):
+        print("Il y a des données pour l'année", i)
+    else:
+        print("Attention ! Les données de l'année", i, "ne sont pas complètes.")
