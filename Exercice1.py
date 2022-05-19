37d1a7badf1336045621327d5dc4a4b883c2eb8b
loditf <101756672+loditf@users.noreply.github.com>
2022-05-19 12:02:38 +0200

    finito pipo 2

--- Exercice1.py
+++ Exercice1.py
@@ -2,17 +2,41 @@
 
 
 import pandas as pd
+
 #importation du fichier csv pour convertir en dataframe et traiter les données
 df_chomage = pd.read_csv("https://www.bfs.admin.ch/bfsstatic/dam/assets/20964153/master", sep = ';')
+
 #remplacer les codes géographiques dans le dataframe
 geo = {'CH':'Suisse','CH011':'Vaud','CH012':'Valais','CH013':'Genève','CH021':'Berne','CH022':'Fribourg','CH023':'Soleure','CH024':'Neuchâtel','CH025':'Jura','CH031':'Bâle-Ville','CH032':'Bâle-Campagne','CH033':'Argovie','CH040':'Zurich','CH051':'Glaris','CH052':'Schaffhouse','CH053':'Appenzell Rhodes-Extérieures','CH054':'Appenzell Rhodes-Intérieures','CH055':'Saint-Gall','CH056':'Grisons','CH057':'Thurgovie','CH061':'Lucerne','CH062':'Uri','CH063':'Schwyz','CH064':'Obwald','CH065':'Nidwald','CH066':'Zoug','CH070':'Tessin'}
 df_chomage = df_chomage.replace(geo)
+
 #remplacer les codes unité dans le dataframe
 unit = {'pers':'Nombre de personnes','HH':'Nombre de ménages', 'pers in %':'Pourcentage de personnes'}
 df_chomage = df_chomage.replace(unit)
+
 #remplacer les codes Obs_Status dans le dataframe
 obs_status = {'A':'Valeur normales','L':'Valeur confidentielle','U':'Faible fiabilité'}
 df_chomage = df_chomage.replace(obs_status)
+
+#remplacer les codes POP1564
+df_pop1564 = df_chomage['POP1564']
+pop = {'1':'Pop. résid. perm. de 15 à 64 ans'}
+df_pop1564 = df_pop1564.replace(pop)
+df_chomage['POP1564']=df_pop1564
+
+#remplacer les codes ERWL
+df_erwl = df_chomage['ERWL']
+erwl = {'0':'Autres personnes','1': 'Personnes au chômage'}
+df_erwl = df_erwl.replace(erwl)
+df_chomage['ERWL']=df_erwl
+
+#remplacer les codes ERWP dans le dataframe
+df_erwp = df_chomage['ERWP']
+erwp = {'0':'Autres personnes','1':'Personnes actives'}
+df_erwp = df_erwp.replace(erwp)
+df_chomage['ERWP'] = df_erwp
+
+
 
 #verfier s'il y a des données pour toutes les années (2010 à 2020)
 for i in range(2010, 2021):
@@ -22,12 +46,14 @@ for i in range(2010, 2021):
         print("Attention ! Les données de l'année", i, "ne sont pas complètes.")
         
 
-        
+print(df_chomage.query("UNIT_MEA =='Nombre de ménages'"))   
+
+
 ###         PARTIE 2
 #traitement données canton de Vaud
 df_vaud = df_chomage.query("GEO=='Vaud'")
 
-chomeurs_num= df_vaud.query("ERWL=='1' and UNIT_MEA =='Nombre de personnes'")
+chomeurs_num= df_vaud.query("ERWL=='Personnes au chômage' and UNIT_MEA =='Nombre de personnes'")
 liste_chomeurs = chomeurs_num['OBS_VALUE'].tolist()
 
 Population = df_vaud.query("POP1564=='Total' and ERWP =='Total' and UNIT_MEA=='Nombre de personnes'")
@@ -58,6 +84,7 @@ plt.ylabel('Nombre de personnes')
 plt.plot(annees, liste_chomeurs, color = 'r')
 plt.show()
 
+
 ##  PARTIE 3
 ## a partir de cette étape, les données sur la population active ne sont plus utiles
 df_chomage = df_chomage.drop(columns=['ERWP'])
@@ -68,8 +95,8 @@ from ipywidgets import widgets
 a = widgets.RadioButtons(options = ['EN', 'FR', 'DE','IT'], description = 'Langue:', disabled = False)
 b = widgets.IntSlider(min = 2010, max = 2020, step = 1, description = 'time', disabled = False, orientation = 'horizontal')
 c = widgets.Dropdown(options = ['Nombre de personnes', 'Nombre de ménages','Pourcentage de personnes'], description = 'unit', disabled = False)
-d = widgets.Dropdown(options = ['1', 'Total'], description = 'population')
-e = widgets.Dropdown(options = ['0', '1', 'Total'], description = 'chomage')
+d = widgets.Dropdown(options = ['Pop. résid. perm. de 15 à 64 ans', 'Total'], description = 'population')
+e = widgets.Dropdown(options = ['Autres personnes', 'Personnes au chômage', 'Total'], description = 'chomage')
 #affichage des widgets
 display(a)
 display(b)
@@ -95,8 +122,10 @@ unité = c.value
 population = d.value
 chomage = e.value
 
-
-df_chomage_precis = df_chomage.query("TIME_PERIOD==@annee and UNIT_MEA == @unité and POP1564 == @population and ERWL == @chomage")
+if unité == 'Nombre de ménages':
+    print("Désolé nous n'avons pas de données pour cette population")
+else:
+    df_chomage_precis = df_chomage.query("TIME_PERIOD==@annee and UNIT_MEA == @unité and POP1564 == @population and ERWL == @chomage")
 
 cantons_dict = {'Zoug': 'kt-zg', 'Vaud': 'kt-vd', 'Valais': 'kt-vs', 'Genève': 'kt-ge', 'Berne': 'kt-be',
                 'Fribourg': 'kt-fr', 'Soleure': 'kt-so', 'Neuchâtel': 'kt-ne', 'Jura': 'kt-ju', 'Bâle-Ville': 'kt-bs',
@@ -108,8 +137,8 @@ cantons_dict = {'Zoug': 'kt-zg', 'Vaud': 'kt-vd', 'Valais': 'kt-vs', 'Genève': 
 df_chomage_fin = df_chomage_precis[['GEO','OBS_VALUE']].replace(cantons_dict)
 
 
-liste_code = df_chomage_cool['GEO']
-liste_valeur = df_chomage_cool['OBS_VALUE']
+liste_code = df_chomage_fin['GEO']
+liste_valeur = df_chomage_fin['OBS_VALUE']
 
 
 
@@ -128,8 +157,7 @@ display(HTML(base_html.format(rendered_chart=carte_suisse)))
 
 
 #     reste à faire:
-# remplacer 1 et 0
 # rendre le code joli
-# jouter les titres
+# ajouter les titres
 # interact
 # LANGUE
